2856dbc40532b9400e2854eb58946c76872541b0
rahisi <mbogod@rahisibet.com>
2020-01-25 11:54:56 +0300

    added method on the calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -11,4 +11,4 @@ class TestCalculator:
         assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
-    assert 100 == calculator.multiply(10, 10)
+        assert 100 == calculator.multiply(10, 10)
